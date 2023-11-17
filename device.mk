0e57932dd73132e2a3af553d970b59c076f4e4d9
redznn <h908659@gmail.com>
2023-11-17 17:19:18 -0300

    spes: Revert dolby changes Signed-off-by: redznn <h908659@gmail.com>

--- device.mk
+++ device.mk
@@ -215,9 +215,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Dolby
-TARGET_EXCLUDES_AUDIOFX := true
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
