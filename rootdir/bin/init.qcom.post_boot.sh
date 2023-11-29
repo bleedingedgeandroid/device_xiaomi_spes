765b011e63f903dad49d4158d4b2b95654e94dc5
redznn <h908659@gmail.com>
2023-11-29 19:04:17 -0300

    spes: Fixup 6ff499d
    Signed-off-by: redznn <h908659@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -77,16 +77,12 @@ echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 # configure governor settings for little cluster
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
 echo 1516800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
 echo 691200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rtg_boost_freq
 
 # configure governor settings for big cluster
 echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
 echo 1344000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
 echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
